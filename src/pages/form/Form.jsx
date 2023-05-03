82093aa85442babd423039f1870cfe7233d181ea
Davron01~ <d.mirabdullayev@mail.ru>
2023-05-03 22:56:19 +0500

    input required

--- src/pages/form/Form.jsx
+++ src/pages/form/Form.jsx
@@ -10,16 +10,17 @@ const Form = () => {
       <h1>Apply Now</h1>
       <form className={styles.form}>
         <h3>Admission Form</h3>
-        <input type="text" name="fullName" placeholder="Full Name" />
-        <input type="text" name="lastName" placeholder="Last Name" />
-        <input type="phone" name="phone" placeholder="mobile" />
-        <input type="email" name="email" placeholder="Your Email" />
+        <input type="text" name="fullName" placeholder="Full Name" required />
+
+        <input type="text" name="lastName" placeholder="Last Name" required />
+        <input type="phone" name="phone" placeholder="mobile" required />
+        <input type="email" name="email" placeholder="Your Email" required />
         <input
           type="date"
           name="date"
           placeholder="Date of birth (DD/MM/YYYY)"
+          required
         />
-
         <select name="Applying" id="cars">
           <option value="Applying School Grade" disabled selected>
             Applying School Grade
@@ -28,7 +29,6 @@ const Form = () => {
           <option value="Ibn Sino Branch">Ibn Sino Branch</option>
           <option value="Samarqand Branch">Samarqand Branch</option>
         </select>
-
         <select name="level" id="level">
           <option value="English Level" disabled selected>
             English Level
@@ -38,7 +38,6 @@ const Form = () => {
           <option value="intermediate">intermediate</option>
           <option value="advanced">advanced</option>
         </select>
-
         <select name="branch" id="branch">
           <option value="Select Branch" disabled selected>
             Select Branch
@@ -47,14 +46,20 @@ const Form = () => {
           <option value="Ibn-Sino Branch">Ibn Sino Branch</option>
           <option value="Samarqand Branch">Samarqand Branch</option>
         </select>
-        <input type="text" name="Region" placeholder="Region" />
-        <input type="text" name="Adress" placeholder="Adress" />
+        <input type="text" name="Region" placeholder="Region" required />
+        <input type="text" name="Adress" placeholder="Adress" required />
         <input
           type="text"
           name="Parents Full Name"
           placeholder="Parents' Full Name"
+          required
         />
-        <input type="tel" name="Parents Mobile" placeholder="Parents Mobile" />
+        <input
+          type="tel"
+          name="Parents Mobile"
+          placeholder="Parents Mobile"
+          required
+        />
         <button>Submit</button>
       </form>
     </div>
